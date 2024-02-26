6f94a1a7368a37b0c7c9b3fbfd65457a19d576bd
Yeongu Choe <yeongu.choe@icloud.com>
2024-02-26 13:32:37 +0900

    fix

--- main.py
+++ main.py
@@ -8,12 +8,13 @@ import asyncio
 
 async def main():
     t = read_access_token()
-    print(get_current_price1(t,"001210"))
+    print(get_current_price1(t, "001210"))
     # sell(t, "001210", "938", "1")
     # cancel_order(t, "0000111426")
     # print(list_sell_order(t))
     # print(get_remaining_cash(t))
     # print(get_remaining_stock(t))
+
 
 if __name__ == "__main__":
     asyncio.run(main())
