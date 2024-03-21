c008ac3cdd36c87d620009d2541bd5831eeee84d
Yeongu Choe <yeongu.choe@icloud.com>
2024-03-21 18:39:27 +0900

    update credential

--- credential.py
+++ credential.py
@@ -1,9 +1,9 @@
-CANO = "64098841"  # Account number
+CANO = "64000000"  # Account number
 ACNT_PRDT_CD = "01" # Account product number (Leave it like this)
-APP_Key = "PSsAP2ThHPnTYd5NKZMYRfBpeiklCEXp7CiU" # App key from the website
-APP_Secret = "xe+1DHXqm8MIYatEqEqKQiFgI0pY8UXpFJo8xrqBUKdXTSy1X+a4ucxIRz1Mrwkq3C2OadCN1FTkiMWE1pJXAh1yBOG8vtgeKxDDopcxilI4a8OGk9/Qsf2e6crPJ+tyuCS9OXJrD5/+Fm3MoxStItxJPdzrcJq7fV1dGrNYHCvl1BHvwGI=" # Approval key from website
+APP_Key = "123456789" # App key from the website
+APP_Secret = "123456789" # Approval key from website
 
 # URL and port to connect to trading server
-url = "https://openapi.koreainvestment.com:9443"
+url = "https://openapi.koreainvestment.com:9443" # (Leave it like this)
 # URL and port to connect to websocket server
-WebSocket_url = "ws://ops.koreainvestment.com:21000"
+WebSocket_url = "ws://ops.koreainvestment.com:21000" # (Leave it like this)
